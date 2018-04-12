1fcb8c4f883f9cb4e250606911a7b927cda6ec96
omer barak <omer.barak@live.biu.ac.il>
2018-04-12 19:17:24 +0300

    finish

--- Excercise 2/ex2.py
+++ Excercise 2/ex2.py
@@ -3,37 +3,82 @@ import matplotlib.pyplot as plt
 
 
 def phi(x, miu):
+    """
+    Calculate the phi function for x and miu.
+    :param x:
+    :param miu:
+    :return:
+    """
     return 1 / np.sqrt(2 * np.pi) * np.exp((-(x - miu) ** 2) / 2)
 
 
 def softmax(x):
+    """
+    Calculate the softmax of x
+    :param x: wx+b
+    :return: softmax of (x)
+    """
     ex = np.exp(x - np.max(x))
     return ex / ex.sum()
 
 
-def generate_training_data(tags):
+def generate_training_data(tags, no_examples=100):
+    """
+    Generate training data.
+    :param tags: tags array
+    :param no_examples: number of examples.
+    :return: zip of training set and y
+    """
     training_set = []
     y = []
     for tag in tags:
-        fxy = np.random.normal(2 * tag, 1, 100)
+        fxy = np.random.normal(2 * tag, 1, no_examples)  # f(x | y = a) = N(2a, 1), a = 1, 2, 3
         training_set.extend(fxy)
-        y.extend([tag] * 100)
-    return zip(y, training_set)
+        y.extend([tag] * no_examples)
+    return zip(training_set, y)
 
 
-def train(set, weights, bias, learn_rate=0.1):
-    pass
+def train(training_set, w, b, epochs=10, learn_rate=0.1):
+    """
+    Train the data.
+    :param training_set: training set
+    :param w: weights
+    :param b: bias
+    :param epochs: number of epochs
+    :param learn_rate: learning rate
+    :return: learned weights and bias
+    """
+    for i in range(epochs):
+        np.random.shuffle(training_set)
+        for x, y in training_set:
+            sftmx = softmax(w * x + b)
+            dw = x * sftmx
+            dw[y - 1] = x * sftmx[y - 1] - x
+            db = sftmx
+            db[y - 1] = sftmx[y - 1] - 1
+            w = w - learn_rate * dw  # w^t = w^(t-1) - eta*dw
+            b = b - learn_rate * db  # b^t = b^(t-1) - eta*db
+    return w, b
 
 
-def plot(tags):
-    density = np.linspace(0, 10, 100)  # Graph density
-
+def plot(tags, w, b, start=0, end=10, den=100):
+    """
+    Plot the graphs
+    :param tags:
+    :param w:
+    :param b:
+    :param start:
+    :param end:
+    :param den:
+    :return:
+    """
+    density = np.linspace(start, end, den)  # Draw the graph for x in the range [0,10]  (+ density)
     true_distribution = []
     test = []
     for x in density:
-        cdf = [phi(x, 2 * label) for label in tags]
+        cdf = [phi(x, 2 * tag) for tag in tags]
         true_distribution.append(cdf[0] / sum(cdf))
-        test.append(cdf[1] / sum(cdf))
+        test.append(softmax(w * x + b)[0])
 
     # Graph styling #
     plt.rcParams.update({'font.size': 9})
@@ -51,14 +96,14 @@ def plot(tags):
     plt.show()
 
 
-def main():
-    tags = [1, 2, 3]
+def main(tags=None):
+    if tags is None:
+        tags = [1, 2, 3]
     training_set = generate_training_data(tags)
-
-    weights = np.zeros(3)
-    bias = np.zeros(3)
-    train(training_set, weights, bias)
-    plot(tags)
+    weights = np.random.random((len(tags), 1))
+    bias = np.random.random((len(tags), 1))
+    w, b = train(training_set, weights, bias)
+    plot(tags, w, b)
 
 
 if __name__ == '__main__':
