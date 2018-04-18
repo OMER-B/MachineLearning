021176305abcb4103c9ed65b007f6f51b841f2dc
omer barak <omer.barak@live.biu.ac.il>
2018-04-18 11:51:29 +0300

    added comments

--- Excercise 2/ex2.py
+++ Excercise 2/ex2.py
@@ -2,51 +2,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def phi(x, miu):
+def f(x, miu):
     """
-    Calculate the phi function for x and miu.
-    :param x:
-    :param miu:
-    :return:
+    Calculate the probability function for x and miu.
+    :param x: x to calculate for.
+    :param miu: Mean of the distribution.
+    :return: Probability density of the normal distribution.
     """
     return 1 / np.sqrt(2 * np.pi) * np.exp((-(x - miu) ** 2) / 2)
 
 
 def softmax(x):
     """
-    Calculate the softmax of x
-    :param x: wx+b
-    :return: softmax of (x)
+    Calculate the softmax of x (= wx + b)
+    :param x: Should be (w*x+b)
+    :return: Softmax result of x
     """
     ex = np.exp(x - np.max(x))
     return ex / ex.sum()
 
 
-def generate_training_data(tags, no_examples=100):
+def generate_training_data(Classes, no_examples=100):
     """
-    Generate training data.
-    :param tags: tags array
-    :param no_examples: number of examples.
-    :return: zip of training set and y
+    Generate training data for the classes.
+    :param Classes: Array of classes.
+    :param no_examples: Number of examples to generate for each class. The default is 100 examples.
+    :return: Array of tuples of the form (x, y).
     """
     training_set = []
     y = []
-    for tag in tags:
+    for tag in Classes:
         fxy = np.random.normal(2 * tag, 1, no_examples)  # f(x | y = a) = N(2a, 1), a = 1, 2, 3
         training_set.extend(fxy)
         y.extend([tag] * no_examples)
     return zip(training_set, y)
 
 
-def train(training_set, w, b, epochs=10, learn_rate=0.1):
+def train(training_set, w, b, epochs=200, etaw=0.01, etab=0.01):
     """
-    Train the data.
-    :param training_set: training set
-    :param w: weights
-    :param b: bias
-    :param epochs: number of epochs
-    :param learn_rate: learning rate
-    :return: learned weights and bias
+    Train the data set.
+    :param training_set: Set to train.
+    :param w: Weights to train by.
+    :param b: Bias to train by.
+    :param epochs: Number of epochs for the training. Default is 200.
+    :param etaw: Learn rate of w. Default is 0.01.
+    :param etab: Learn rate of b. Default is 0.01.
+    :return: Vectors of trained weights and bias.
     """
     for i in range(epochs):
         np.random.shuffle(training_set)
@@ -56,28 +57,28 @@ def train(training_set, w, b, epochs=10, learn_rate=0.1):
             dw[y - 1] = x * sftmx[y - 1] - x
             db = sftmx
             db[y - 1] = sftmx[y - 1] - 1
-            w = w - learn_rate * dw  # w^t = w^(t-1) - eta*dw
-            b = b - learn_rate * db  # b^t = b^(t-1) - eta*db
+            w = w - etaw * dw  # w^t = w^(t-1) - eta*dw
+            b = b - etab * db  # b^t = b^(t-1) - eta*db
     return w, b
 
 
-def plot(tags, w, b, start=0, end=10, den=100):
+def plot(classes, w, b, start=0, end=10, den=100):
     """
-    Plot the graphs
-    :param tags:
-    :param w:
-    :param b:
-    :param start:
-    :param end:
-    :param den:
+    Plots the probability density graph and the learned probability graph.
+    :param classes: Classes to draw by.
+    :param w: Weights.
+    :param b: Bias.
+    :param start: Start point of the graph. Default is from 0.
+    :param end: End point of the graph. Default is to 10.
+    :param den: Density to draw. Default is 100 points.
     :return:
     """
     density = np.linspace(start, end, den)  # Draw the graph for x in the range [0,10]  (+ density)
     true_distribution = []
     test = []
     for x in density:
-        cdf = [phi(x, 2 * tag) for tag in tags]
-        true_distribution.append(cdf[0] / sum(cdf))
+        pdf = [f(x, 2 * c) for c in classes]
+        true_distribution.append(pdf[0] / sum(pdf))
         test.append(softmax(w * x + b)[0])
 
     # Graph styling #
@@ -89,21 +90,21 @@ def plot(tags, w, b, start=0, end=10, den=100):
     plt.minorticks_on()
     plt.tick_params(direction='out', color='black')
     plt.grid(color='black', alpha=0.01, linewidth=0.3, which='both')
-    plt.plot(density, true_distribution, color='y', linestyle='dashed', linewidth=1)
+    plt.plot(density, true_distribution, color='y', linestyle='dotted', linewidth=1)
     plt.plot(density, test, color='purple', linewidth=2)
     plt.legend(('True', 'Estimated'),
                fancybox=False, edgecolor='white', fontsize='small')
     plt.show()
 
 
-def main(tags=None):
-    if tags is None:
-        tags = [1, 2, 3]
-    training_set = generate_training_data(tags)
-    weights = np.random.random((len(tags), 1))
-    bias = np.random.random((len(tags), 1))
+def main(classes=None):
+    if classes is None:
+        classes = [1, 2, 3]
+    training_set = generate_training_data(classes)
+    weights = np.random.random((len(classes), 1))
+    bias = np.random.random((len(classes), 1))
     w, b = train(training_set, weights, bias)
-    plot(tags, w, b)
+    plot(classes, w, b)
 
 
 if __name__ == '__main__':
